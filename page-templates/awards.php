3e08ab34d6e1118843c3fe5244788b4ffc6a0d78
Casper van Schooten <casper@webbakery.nl>
2022-02-23 14:38:49 +0100

    Award titel fix

--- page-templates/awards.php
+++ page-templates/awards.php
@@ -83,7 +83,7 @@ get_header();
                     <div class="row">
                         <div class="col-sm-12 col-lg-8 offset-lg-2">
                             <div class="content d-flex align-items-center justify-content-center">
-                                <h3><?php the_field('titel', 'options'); ?></h3>
+                                <h3><?php the_field('titel'); ?></h3>
                                 <?php
                                 $url = get_field( 'button_link' );
                                 if ( $url ) : ?>
