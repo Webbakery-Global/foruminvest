4c0f566cd08a652e60190ec35657dcb182afc372
Casper van Schooten <casper@webbakery.nl>
2022-02-25 12:12:48 +0100

    Fix Team slider

--- assets/js/foruminvest.js
+++ assets/js/foruminvest.js
@@ -72,13 +72,15 @@ $(document).ready(function () {
             breakpoint: 1024,
             settings: {
                 slidesToShow: 2,
+                slidesToScroll: 2,
             }
         },
             {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 1,
-
+                    slidesToScroll: 1,
+                    dots: false,
                 }
             },
         ]
