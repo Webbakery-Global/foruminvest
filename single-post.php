7fafdf09fa06ff9bb8501740279e68108de447a8
caspervanschooten <casper@webbakery.nl>
2022-03-08 16:56:58 +0100

    Fix post

--- single-post.php
+++ single-post.php
@@ -14,7 +14,6 @@ $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-lg-10">
-                <p class="date"><?php echo get_the_date('d-m-Y'); ?></p>
                 <h1 class="title mb-5"><?php the_title(); ?></h1>
                 <?php the_content(); ?>
             </div>
